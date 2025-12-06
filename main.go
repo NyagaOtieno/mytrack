dca691a2abf262628f772cd9d48bec35b1f1ed8c
Kennedy Otieno <nyagaotieno@gmail.com>
2025-12-05 22:45:42 -0800

    Updated track.go and fixed backend API 401 issue

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func corsMiddleware(next http.Handler) http.Handler {
 		w.Header().Set("Vary", "Origin")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-API-Key")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, )
 
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
